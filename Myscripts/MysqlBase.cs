01f0cbe6e2d7e319729d57736bd68937c134103f
Star-tears <1370443225@qq.com>
2022-12-01 06:43:53 +0800

    chore: 添加Settings类配置全局变量

---
+++ Myscripts/MysqlBase.cs
@@ -0,0 +1,152 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkToDo.Myscripts
+{
+    internal class MysqlBase
+    {
+        private MySqlConnection conn = null;
+        private MySqlCommand command = null;
+        private MySqlDataReader reader = null;
+        
+        /// <summary>
+        /// 构造方法里建议连接
+        /// </summary>
+        /// <param name="connstr"></param>
+        public MysqlBase()
+        {
+            string connstr = "Database=" + Settings.DatbaseName + ";Data Source="+Settings.DatabaseHost+";User Id="+Settings.DatabaseUsername+";Password="+Settings.DatabasePassword+";pooling=false;CharSet=utf8;port="+Settings.DatabasePort+";";
+            conn = new MySqlConnection(connstr);
+        }
+        /// <summary>
+        /// 检测数据库连接状态
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckConnectStatus()
+        {
+            bool result = false;
+            try
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    result = true;
+                }
+
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+        /// <summary>
+        /// 增、删、改公共方法
+        /// </summary>
+        /// <returns></returns>
+        public int commonExecute(string sql)
+        {
+            int res = -1;
+            try
+            {
+                //当连接处于打开状态时关闭,然后再打开,避免有时候数据不能及时更新
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                conn.Open();
+                command = new MySqlCommand(sql, conn);
+                res = command.ExecuteNonQuery();
+            }
+            catch (MySqlException)
+            {
+            }
+            conn.Close();
+            return res;
+        }
+        /// <summary>
+        /// 查询方法
+        /// 注意：尽量不要用select * from table表（返回的数据过长时，DataTable可能会出错），最好指定要查询的字段。
+        /// </summary>
+        /// <returns></returns>
+        public DataTable query(string sql)
+        {
+            //当连接处于打开状态时关闭,然后再打开,避免有时候数据不能及时更新
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            command = new MySqlCommand(sql, conn);
+            DataTable dt = new DataTable();
+            using (reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+            {
+                dt.Load(reader);
+            }
+            return dt;
+        }
+        /// <summary>
+        /// 获取DataSet数据集
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="tablename"></param>
+        /// <returns></returns>
+        public DataSet GetDataSet(string sql, string tablename)
+        {
+            //当连接处于打开状态时关闭,然后再打开,避免有时候数据不能及时更新
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            command = new MySqlCommand(sql, conn);
+            DataSet dataset = new DataSet();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            adapter.Fill(dataset, tablename);
+            conn.Close();
+            return dataset;
+        }
+        /// <summary>
+        /// 实现多SQL语句执行的数据库事务
+        /// </summary>
+        /// <param name="SQLStringList">SQL语句集合（多条语句）</param>
+        public bool ExecuteSqlTran(List<string> SQLStringList)
+        {
+            bool flag = false;
+            //当连接处于打开状态时关闭,然后再打开,避免有时候数据不能及时更新
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            //开启事务
+            MySqlTransaction tran = this.conn.BeginTransaction();
+            cmd.Transaction = tran;//将事务应用于CMD
+            try
+            {
+                foreach (string strsql in SQLStringList)
+                {
+                    if (strsql.Trim() != "")
+                    {
+                        cmd.CommandText = strsql;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                tran.Commit();//提交事务（不提交不会回滚错误）
+                flag = true;
+            }
+            catch (Exception)
+            {
+                tran.Rollback();
+                flag = false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return flag;
+        }
+    }
+}
---
+++ Myscripts/QiniuBase.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using Qiniu.Http;
+using Qiniu.Storage;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LinkToDo.Myscripts
+{
+    internal class QiniuBase
+    {
+        public static async void UploadImg(string img_local_path,string target_img_name)
+        {
+            string AccessKey = Settings.QiniuAccessKey;
+            string SecretKey = Settings.QiniuSecretKey;
+            Mac mac = new Mac(AccessKey, SecretKey);
+            // 上传文件名
+            string key = "hznu/class-img-bed/"+target_img_name;
+            // 本地文件路径
+            string filePath = img_local_path;
+            // 存储空间名
+            string Bucket = "star-tears";
+            // 设置上传策略
+            PutPolicy putPolicy = new PutPolicy();
+            // 设置要上传的目标空间
+            putPolicy.Scope = Bucket;
+            // 上传策略的过期时间(单位:秒)
+            putPolicy.SetExpires(3600);
+            //// 文件上传完毕后，在多少天后自动被删除
+            //putPolicy.DeleteAfterDays = 1;
+            // 生成上传token
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            Config config = new Config();
+            // 设置上传区域
+            config.Zone = Zone.ZoneCnEast;
+            // 设置 http 或者 https 上传
+            config.UseHttps = true;
+            config.UseCdnDomains = true;
+            config.ChunkSize = ChunkUnit.U512K;
+            // 表单上传
+            FormUploader target = new FormUploader(config);
+            HttpResult result = await target.UploadFile(filePath, key, token, null);
+            Console.WriteLine("form upload result: " + result.ToString());
+        }
+        public static async void DeleteImg(string target_img_name)
+        {
+            string AccessKey = Settings.QiniuAccessKey;
+            string SecretKey = Settings.QiniuSecretKey;
+            Mac mac = new Mac(AccessKey, SecretKey);
+            // 删除文件名
+            string key = "hznu/class-img-bed/" + target_img_name;
+            Console.WriteLine(key);
+            // 存储空间名
+            string Bucket = "star-tears";
+            // 设置存储区域
+            Config config = new Config();
+            config.Zone = Zone.ZoneCnEast;
+            BucketManager bucketManager = new BucketManager(mac, config);
+            HttpResult deleteRet = await bucketManager.Delete(Bucket, key);
+            Console.WriteLine("delete error: " + deleteRet.ToString());
+            if (deleteRet.Code != (int)HttpCode.OK)
+            {
+                Console.WriteLine("delete error: " + deleteRet.ToString());
+            }
+        }
+    }
+}
